5132002c5534b83a526919fe7f3e4a2976190055
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-03-27 18:30:34 -0600

    Update unbanchat.js

--- plugins/unbanchat.js
+++ plugins/unbanchat.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   global.DATABASE._data.chats[m.chat].isBanned = false
-  m.reply('Done!')
+  m.reply('Ya wn .-.!')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
