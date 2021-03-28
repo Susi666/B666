e2f42e48aeb3fa47ff1c035623f1231b6d55f9c0
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-03-27 21:52:42 -0600

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,21 +1,14 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Smartfren [088235435804]
-│ • Telkomsel [081357302007]
-│ • Gopay [081357302007]
+╭─「 Mis canciones fav xdddddd 」
+│ • Hasta Abajo
+│ • Canción Con Yandel
+│ • Antes Que Se Acabe
 ╰────
-╭─「 Hubungi 」
-│ > Ingin donasi? Wa.me/6281357302007
+╭─「 Mi número 」
+│ > Wa.me/50364274191
 ╰────
 
-Ini *#caranya untuk Donasi*
-*Cara Donasi*:
-1.) Beli ke pulsa/ konter terdekat semisal Indomaret
-2.) Bilang ke konter terdekat..
-"Beli pulsa telkomsel"
-3.)Dan terus masukkan nomor kami 081357302007
-4.) Jika sudah kirim bukti ke sini.. Terimakasih
-*Kalau tidak juga gak papa*👍🏻
+No c que poner acá :v
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
