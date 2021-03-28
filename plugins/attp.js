dc5732b45809238a897602d56fb9da435484dfc8
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-03-27 22:04:08 -0600

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -6,7 +6,7 @@ let handler  = async (m, { conn, text }) => {
   if (text) conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text }), 'attp.webp', '', m, false, { asSticker: true })
   else throw 'Uhm...Teksnya?'
 }
-handler.help = ['attp <teks>']
+handler.help = ['attp <Escribe algo y el bot lo hará sticker>']
 handler.tags = ['sticker']
 handler.command = /^attp$/i
 handler.owner = false
