f53789e8a219ae0439ad6197a46be607d7d35a7d
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-03-27 18:24:52 -0600

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
-  this.sendContact(m.chat, '6288235435804', 'Admin DrawlNag', m)
+  // this.sendContact(m.chat, '642766660666', 'Susi', m)
+  this.sendContact(m.chat, '642766660666', 'My Owner BOT', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
