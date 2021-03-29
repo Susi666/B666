8615cbdd1f6f93c96d6d1714ea8f8e33280233c3
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-03-28 22:50:43 -0600

    Update fetch.js

--- plugins/fetch.js
+++ plugins/fetch.js
@@ -12,7 +12,7 @@ let handler = async (m, { text }) => {
     m.reply(txt.slice(0, 65536) + '')
   }
 }
-handler.help = ['fetch', 'get'].map(v => v + ' <url>')
+handler.help = ['fetch', 'get'].map(v => v + ' <pega la url y mira la magia>')
 handler.tags = ['internet']
 handler.command = /^(fetch|get)$/i
 
