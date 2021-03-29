29a5c941cb9e90d9745e684dd3ba09124c4183d0
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-03-29 11:14:59 -0600

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -1,29 +1,33 @@
 let limit = 30
-let yts = require('yt-search')
-const { servers, yta, ytv } = require('../lib/y2mate')
-let handler = async (m, { conn, command, text, isPrems, isOwner }) => {
-  if (!text) throw 'Cari apa?'
-  let results = await yts(text)
-  let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Video/Audio Tidak ditemukan'
-  let { dl_link, thumb, title, filesize, filesizeF} = await (/2$/.test(command) ? ytv : yta)(vid.url, 'id4')
+const { servers, yta } = require('../lib/y2mate')
+let handler = async (m, { conn, args, isPrems, isOwner }) => {
+  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  let server = (args[1] || 'id4').toLowerCase()
+  let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : 'id4')
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
 *Title:* ${title}
 *Filesize:* ${filesizeF}
-*Source:* ${vid.url}
 *${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
 `.trim(), m)
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp' + (3 + /2$/.test(command)), `
+  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
 *Title:* ${title}
 *Filesize:* ${filesizeF}
-*Source:* ${vid.url}
 `.trim(), m)
 }
-handler.help = ['play', 'play2'].map(v => 'yt' + v + ' <pencarian>')
+handler.help = ['mp3','a'].map(v => 'yt' + v + ' <url> [server: id4, en60]')
 handler.tags = ['downloader']
-handler.command = /^play2?$/i
+handler.command = /^yt(a|mp3)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
 
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
 handler.exp = 0
 handler.limit = true
 
