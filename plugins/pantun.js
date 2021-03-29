4e223a5dedafea95d11ed9dfedccf10cbdf7c14c
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-03-28 22:36:09 -0600

    Update pantun.js

--- plugins/pantun.js
+++ plugins/pantun.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn }) => {
   conn.reply(m.chat,`“${pickRandom(global.pantun)}”`, m)
 }
-handler.help = ['pantun']
+handler.help = ['pantun <Frases que robe de canciones xd>']
 handler.tags = ['quotes']
 handler.command = /^(pantun)$/i
 handler.owner = false
@@ -22,17 +22,17 @@ function pickRandom(list) {
 }
 
 global.pantun = [
-" *PANTUN:*\n\nAda anak kecil bermain batu,\nBatu dilempar masuk ke sumur,\nBelajar itu tak kenal waktu,\nJuga tidak memandang umur.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nTanam kacang di pagi hari,\nTumbuh enam layu sebatang,\nKeburukan orang jangan dicari,\nBila kalian sedang puasa.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nAkhir bulan mendapat gaji,\nGaji untuk membeli ketupat,\nRajin-rajinlah sholat dan mengaji,\nJanganlah lupa puasa dan zakat.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nWaktu daftar hari terakhir,\nWaktu terasa banyak terbuang,\nKamu nggak perlu khawatir,\ncintaku hanya untukmu seorang.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nAda anak kecil bermain batu,\nBatu dilempar masuk ke sumur,\nBelajar itu tak kenal waktu,\nJuga tidak memandang umur.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nSeribu bebek di kandang singa,\nhanya satu berwarna belang,\nBeribu cewek di Indonesia,\nhanya engkau yang aku sayang.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nHari minggu pergi ke pasar,\nBeli sayur dan juga beras,\nTiap hari harus belajar,\nPasti akan menjadi cerdas.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nAyam goreng setengah mateng,\nBelinya di depan tugu.\nAbang sayang, abangku ganteng,\nlneng di sini setia menunggu.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nApi kecil dari tungku,\nApinya kecil habis kayu.\nSudah lama kutunggu-tunggu,\nkapan kamu bilang I love you.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nSeribu bebek di kandang singa,\nhanya satu berwarna belang\nBeribu cewek di Indonesia,\nhanya engkau yang aku sayang.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nPergi memancing saat fajar,\nPulang siang membawa ikan\nSiapa yang rajin belajar\nJadi orang sukses kemudian.\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nBeli computer itu biasa\nSupaya belajar jadi semangat\nMari kita belajar puasa\nAgar kita jadi kuat\n *Bye Instagram:* @arpunchs",
-" *PANTUN:*\n\nMinum sekoteng hangat rasanya,\nminum segelas ada yang minta.\nLaki-laki ganteng siapa yang punya?\nBolehkah aku jatuh cinta.\n *ByeInstagram:* @arpunchs",
+" *PANTUN:*\n\nEs que tiene unos ojos que iluminarían Manhattan,\nY una voz hermosa que mil paisajes retratan,\nAcaso no ves que cuando caminas, desfilas,\nMiremos el cielo hasta que se ponga lila.\n *Sigueme en Instagram:* @susana_monterroza_",
+" *PANTUN:*\n\nPerreando duro, les gusta mi culo,\nHace tiempo te 'toy esperando,\nNo sé tú, pero yo aquí pensando,\nNo haces na' pa' lo que está' roncando.\n *Sigueme en Instagram:* @susana_monterroza_",
+" *PANTUN:*\n\nSiempre hay un amor,\nQue sin ser el primero,\nTe hace olvidar todo,\nEse es el verdadero.\n *Sigueme en Instagram:* @susana_monterroza_",
+" *PANTUN:*\n\nSinceramente solo siento pánico en escena,\nY sostenerte la mirada me quema,\nPero que hoy vivas con mi amor corriendo por tus venas,\nEs por robarme el corazón tu condena.\n *Sigueme en Instagram:* @susana_monterroza_",
+" *PANTUN:*\n\nI know you, you're like this,\nWhen shit don't go your way you needed me to fix it,\nAnd like me, I did,\nBut I ran out of every reason.\n *Sigueme en Instagram:* @susana_monterroza_",
+" *PANTUN:*\n\nA otra persona no he podido amar,\nY te juro que lo he tratado,\nPero es que ninguna se te para al la'o,\nDesde que te fuiste sigo trastorna'o.\n *Sigueme en Instagram:* @susana_monterroza_",
+" *PANTUN:*\n\nTú mataste la' emocione',\nTe fuiste sin darme razone',\nEn la calle te pinta' de santa, pero lo tuyo e' romper corazone',\nAhora tengo má' opcione'.\n *Sigueme en Instagram:* @susana_monterroza_",
+" *PANTUN:*\n\nAntes que se acabe todo esto,\nVoy a vivir mi vida, si me muero e' contento,\nY hoy quiero alcohol y sexo en exceso,\nPa' darte problema' mejor te doy un beso.\n *Sigueme en Instagram:* @susana_monterroza_",
+" *PANTUN:*\n\nTrato de vivir sin ti pero no puedo,\nDe quedarme solo tengo miedo,\nSoy como un niño pequeño en esta oscuridad,\nLa vida no vale prefiero estar muerto.\n *Sigueme en Instagram:* @susana_monterroza_",
+" *PANTUN:*\n\nY ojalá nunca te abracen por última vez,\nHay tantos con quien estar, pero no quien ser\nTan solo somos caminos que suelen torcer,\nMiles de complejos sueltos que debemos de vencer.\n *Sigueme en Instagram:* @susana_monterroza_",
+" *PANTUN:*\n\nMi cabeza está tan rota que no puede más,\nY mi corazón tan negro que no puede amar\nUna lágrima se escapa y rueda por mi piel\nLa culpa es tan solo mía, no de los demás.\n *Sigueme en Instagram:* @susana_monterroza_",
+" *PANTUN:*\n\nSi me dices para ti quién soy\nNo durare en hacerte tan feliz, eres especial para mí\nDime por qué me haces sufrir\nYo te olvidaré desde las Fuentes de Ortiz\n *Sigueme en Instagram:* @susana_monterroza_",
+" *PANTUN:*\n\nEl amor es una balanza,\nPierde el primero que se cansa,\nSi tú quieres la correr la chansa\nSi te dañan, no puedes tomar venganza.\n *Sigueme en Instagram:* @susana_monterroza_",
 ]
