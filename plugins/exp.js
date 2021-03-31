ec222df25890c53e12787302a14348aeac3e5553
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-03-30 18:16:19 -0600

    Update exp.js

--- plugins/exp.js
+++ plugins/exp.js
@@ -7,21 +7,21 @@ let handler = async (m, { conn, text }) => {
   else who = m.chat
   if (!who) throw 'etiqueta a uno'
   let txt = text.replace('@' + who.split`@`[0], '').trim()
-  if (isNaN(txt)) throw 'Hanya angka'
+  if (isNaN(txt)) throw 'Solo números'
   let xp = parseInt(txt)
   let exp = xp
   let pjk = Math.ceil(xp * pajak)
   exp += pjk
   if (exp < 1) throw 'Minimal 1'
   let users = global.DATABASE._data.users
-  if (exp > users[m.sender].exp) throw 'Exp tidak mencukupi untuk mentransfer'
+  if (exp > users[m.sender].exp) throw 'No hay suficiente experiencia para transferir'
   users[m.sender].exp -= exp
   users[who].exp += xp
 
   m.reply(`(${-xp} XP) + (${-pjk} XP (Pajak 2%)) = ( ${-exp} XP)`)
   conn.fakeReply(m.chat, `+${xp} XP`, who, m.text)
 }
-handler.help = ['exp <Pon el monto que quieres>']
+handler.help = ['exp ᴾᵒⁿ ᵉˡ ᵐᵒⁿᵗᵒ ᑫᵘᵉ ᑫᵘᶦᵉʳᵉˢ']
 handler.tags = ['xp']
 handler.command = /^exp$/i
 handler.owner = true
