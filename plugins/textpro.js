2cf341b2ef265cf6429779d83ab7af101eae9acb
Susi666 <81292387+Susi666@users.noreply.github.com>
2021-03-30 18:40:03 -0600

    Update textpro.js

--- plugins/textpro.js
+++ plugins/textpro.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, args: [effect], text: txt }) => {
     throw e
   }
 }
-handler.help = ['textpro'].map(v => v + ' <effect> <text>|[text2]')
+handler.help = ['textpro'].map(v => v + ' ᴱˢᶜʳᶦᵇᵉ ᵘⁿ ᵗᵉˣᵗᵒ ʸ ᵗᵉ ᶜʳᵉᵃ ᵉˡ ᵉᶠᵉᶜᵗᵒ')
 handler.tags = ['tools']
 handler.command = /^(textpro)$/i
 
